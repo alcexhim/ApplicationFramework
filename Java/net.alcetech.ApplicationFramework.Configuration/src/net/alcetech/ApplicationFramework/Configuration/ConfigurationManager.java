6ae7b4d0e5a7ed8f92eed8e138eb807c9b1895b6
Michael Becker <alcexhim@gmail.com>
2016-09-20 12:14:12 -0400

    Enable application to specify configuration file name filter

--- Java/net.alcetech.ApplicationFramework.Configuration/src/net/alcetech/ApplicationFramework/Configuration/ConfigurationManager.java
+++ Java/net.alcetech.ApplicationFramework.Configuration/src/net/alcetech/ApplicationFramework/Configuration/ConfigurationManager.java
@@ -13,12 +13,16 @@ import net.alcetech.UniversalEditor.ObjectModels.Markup.*;
 
 public class ConfigurationManager {
 	
+	private static String _configurationFileNameFilter = "*.xml";
+	public static void setConfigurationFileNameFilter(String value) { _configurationFileNameFilter = value; }
+	public static String getConfigurationFileNameFilter() { return _configurationFileNameFilter; }
+	
 	public static void initialize() {
 		
 		String basePath = "/usr/share/" + Application.getShortName();
 		Directory directory = new Directory(basePath);
 		
-		File[] files = directory.getFiles("*.uexml", true);
+		File[] files = directory.getFiles(getConfigurationFileNameFilter(), true);
 		
 		MarkupObjectModel mom = new MarkupObjectModel();
 		XMLDataFormat xdf = new XMLDataFormat();
