f6db6e968ab8b351c6507ba4face96ae976524c2
Michael Becker <alcexhim@gmail.com>
2016-09-20 00:06:10 -0400

    Change visibility of methods to Public and add contains* methods

--- Java/net.alcetech.ApplicationFramework.Core/src/net/alcetech/ApplicationFramework/Collections/Generic/Dictionary.java
+++ Java/net.alcetech.ApplicationFramework.Core/src/net/alcetech/ApplicationFramework/Collections/Generic/Dictionary.java
@@ -5,12 +5,12 @@ public class Dictionary<T1, T2> {
 	private List<T1> _keys = new List<T1>();
 	private List<T2> _values = new List<T2>();
 	
-	void add(T1 key, T2 value) {
+	public void add(T1 key, T2 value) {
 		_keys.add(key);
 		_values.add(value);
 	}
 	
-	void set(T1 key, T2 value) {
+	public void set(T1 key, T2 value) {
 		boolean found = false;
 		for (int i = 0; i < _keys.count(); i++)
 		{
@@ -23,14 +23,14 @@ public class Dictionary<T1, T2> {
 		}
 		if (!found) add(key, value);
 	}
-	T2 get(T1 key) {
+	public T2 get(T1 key) {
 		for (int i = 0; i < _keys.count(); i++)
 		{
 			if (_keys.get(i).equals(key)) return _values.get(i);
 		}
 		return null;
 	}
-	boolean remove(T1 key) {
+	public boolean remove(T1 key) {
 		boolean found = false;
 		for (int i = 0; i < _keys.count(); i++) {
 			if (_keys.get(i) == key) {
@@ -43,7 +43,14 @@ public class Dictionary<T1, T2> {
 		return found;
 	}
 	
-	int count() {
+	public int count() {
 		return _keys.count();
+	}
+
+	public boolean containsKey(T1 key) {
+		return _keys.contains(key);
+	}
+	public boolean containsValue(T2 value) {
+		return _values.contains(value);
 	}
 }
